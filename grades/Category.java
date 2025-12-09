816e5e972dd370574021c293951a6da891131fbf
Nate Weddle <nateweddle11@gmail.com>
2025-12-09 16:08:03 -0700

    cmdShowCategories updated, cmdAddCategory added

--- grades/Category.java
+++ grades/Category.java
@@ -13,7 +13,8 @@ public class Category {
         String sql =
             "SELECT c.category_name, " +
                     "       c.weight " +
-            "FROM category c ";
+            "FROM class_category c " +
+            "WHERE c.class_id = " + ctx.getActiveClassID();
 
         Connection conn = ctx.getConnection();
         try (PreparedStatement ps = conn.prepareStatement(sql);
@@ -33,15 +34,51 @@ public class Category {
             }
         }
     }
-    }
 
     /**
      * add-category Name weight
-     * 
+     *
      * @param ctx
      * @param args
      * @throws Exception
      */
     public static void cmdAddCategory(ShellContext ctx, String args) throws Exception {
+        String[] parts = args.split("\\s+", 2);
+        if (parts.length != 2) {
+            System.out.println("Usage: add-category NAME WEIGHT");
+            return;
+        }
+
+        String name = parts[0];
+
+        int weight;
+        try {
+            weight = Integer.parseInt(parts[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid weight value: " + parts[1]);
+            return;
+        }
+
+        //Insert category_name into category table if it doesn't already exist
+
+        String sql = "INSERT IGNORE INTO category (name) " + "VALUES (?)";
+
+        Connection conn = ctx.getConnection();
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, name);
+            ps.executeUpdate();
+        }
+
+        sql = "INSERT INTO class_category (class_id, category_name, weight) " + "VALUES (?, ?, ?)";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, ctx.getActiveClassID());
+            ps.setString(2, name);
+            ps.setInt(3, weight);
+            ps.executeUpdate();
+        }
+
+        System.out.println("Added category " + name + "with weight " + weight);
     }
+
 }
