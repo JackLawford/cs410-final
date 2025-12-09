c6b1d028172d704a09ecc59f80e4f6475c193377
Nate Weddle <nateweddle11@gmail.com>
2025-12-09 15:34:39 -0700

    cmdShowCategories complete

--- grades/Category.java
+++ grades/Category.java
@@ -10,6 +10,29 @@ public class Category {
      * @throws Exception
      */
     public static void cmdShowCategories(ShellContext ctx, String args) throws Exception {
+        String sql =
+            "SELECT c.category_name, " +
+                    "       c.weight " +
+            "FROM category c ";
+
+        Connection conn = ctx.getConnection();
+        try (PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            boolean any = false;
+            while (rs.next()) {
+                any = true;
+                String name = rs.getString("category_name");
+                int weight = rs.getInt("weight");
+
+                System.out.printf("%s: %d%%%n", name, weight);
+            }
+
+            if (!any) {
+                System.out.println("No categories found.");
+            }
+        }
+    }
     }
 
     /**
